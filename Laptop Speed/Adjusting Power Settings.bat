3230efec8019c24371503a9e74c19e5f27f3dccc
Shwet-Kamal-Singh <107560088+Shwet-Kamal-Singh@users.noreply.github.com>
2023-04-22 22:01:11 +0530

    Create Adjusting Power Settings.bat

---
+++ Laptop Speed/Adjusting Power Settings.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: This batch file adjusts power settings to optimize for performance.
+
+:: Using the "powercfg.cpl" command to open the Power Options window.
+powercfg.cpl
+
+:: Prompting the user to select a power plan that optimizes for performance.
+
+:: Pausing the batch file to prevent it from closing automatically and to allow the user to select a power plan.
+pause
